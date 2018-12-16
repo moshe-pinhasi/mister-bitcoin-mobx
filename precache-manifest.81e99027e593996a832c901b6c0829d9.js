d00307f084532340f9fd98b8cd2b722a2edd2038
moshe-pinhasi <pi.moshe@gmail.com>
2018-12-16 12:43:28 +0200

    Updates

---
+++ precache-manifest.81e99027e593996a832c901b6c0829d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "95e31bfc136e3fc3f33d",
+    "url": "/static/js/main.95e31bfc.chunk.js"
+  },
+  {
+    "revision": "088130afb853bae06a05",
+    "url": "/static/js/1.088130af.chunk.js"
+  },
+  {
+    "revision": "95e31bfc136e3fc3f33d",
+    "url": "/static/css/main.502f23c2.chunk.css"
+  },
+  {
+    "revision": "a5894b22435b5e0a7b512c2848aa1d95",
+    "url": "/index.html"
+  }
+];
